a81effbdc69e0a19b125a7d4eaa3527159f3f39e
Devlim0210 <ftr.devlim@gmail.com>
2025-05-05 18:37:03 +0200

    Étape 5 : Ajout de submit_contact.php pour traiter les données du formulaire contact

--- submit_contact.php
+++ submit_contact.php
@@ -4,9 +4,10 @@ require 'db_connection.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupération des données du formulaire
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
+    //Securisation des données / utilisation de l'operazteur de coalescence nulle (??)=>pour eviter les erreurs 
+    $name = htmlspecialchars($_POST['name']?? '');
+    $email = htmlspecialchars($_POST['email']?? '');
+    $message = htmlspecialchars($_POST['message']?? '');
 
    
 
@@ -15,11 +16,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
        // Préparer la requête SQL pour insérer les données dans la base de données
        $sql = "INSERT INTO contact (nom, email, message) VALUES (:nom, :email, :message)";
        $stmt = $pdo->prepare($sql);
-       if ($stmt->execute(['nom' => $name, 'email' => $email, 'message' => $message])) {
+       $success = $stmt->execute(['nom' => $name, 'email' => $email, 'message'=>$message ]) ;
+        
+       if ($success) {
         echo "Message enregistré avec succès !";
-    } 
-   } else {
-       echo "Tous les champs doivent être remplis.";
-   }
+    } else {
+        echo "Erreur lors de l'enregistrement du message.";
+    }
+} else {
+    echo "Tous les champs doivent être remplis.";
 }
+} else {
+echo "Méthode non autorisée.";
+}
+  
+
    
